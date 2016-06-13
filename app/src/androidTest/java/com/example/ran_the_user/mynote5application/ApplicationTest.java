365b0b1c501552a80f2ade98d615c12fe4dd795c
surferran <surferran@yahoo.com>
2016-06-14 01:23:33 +0300

    1st version
    just example app

---
+++ app/src/androidTest/java/com/example/ran_the_user/mynote5application/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.ran_the_user.mynote5application;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
